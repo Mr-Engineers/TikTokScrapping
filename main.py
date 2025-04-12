6aaf420fdfb5362e0c7ad8b7b54125a42039a663
Patryk Bindas <bindas.patryk@gmail.com>
2025-04-12 18:15:05 +0200

    added db connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import http.client
 import json
+from pymongo import MongoClient
 
 conn = http.client.HTTPSConnection("tiktok-creative-center-api.p.rapidapi.com")
 
@@ -13,7 +14,37 @@ conn.request("GET", "/api/trending/hashtag?page=1&limit=20&period=120&country=US
 res = conn.getresponse()
 data = res.read()
 
-# # Parse JSON string to dict
 jsonData = json.loads(data.decode("utf-8"))
 
-print(json.dumps(jsonData, indent=2))
+trend_list = jsonData["data"]["list"]
+
+uri = "mongodb+srv://root:root@hacknarok.quwkjxf.mongodb.net/?retryWrites=true&w=majority&appName=Hacknarok"
+
+client = MongoClient(uri)
+db = client["Hacknarok"]
+collection = db["Tiktok"]
+
+for trend in trend_list:
+    hashtag_name = trend.get("hashtag_name") or None
+    country = trend.get("country_info", {}).get("value") or None
+    is_promoted = trend.get("is_promoted") or False
+    timestamp = trend.get("trend") or []
+    creators = trend.get("creators") or []
+    publish_count = trend.get("publish_cnt") or 0
+    video_views = trend.get("video_views") or 0
+
+    all_nicknames = []
+    for creator in creators:
+        all_nicknames.append(creator.get("nick_name"))
+
+    print(hashtag_name, country, is_promoted, all_nicknames, publish_count, video_views)
+
+    collection.insert_one({
+        "title": hashtag_name,
+        "country": country,
+        "is_promoted": is_promoted,
+        "creators": all_nicknames,
+        "publish_count": publish_count,
+        "video_views": video_views,
+        "timestamps": timestamp,
+    })
